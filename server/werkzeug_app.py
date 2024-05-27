2b474c320441da658f35218647b388376dac04e8
+ <+>
2024-05-27 21:01:32 +0300

    Completed Lab

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,20 @@
-#!/usr/bin/env python3
+from werkzeug.wrappers import Request, Response
+
+# add function to tell the server to run any request code coming from our browser 
+@ Request.application
+def application(request):
+    print (f'This web server  is running at {request.remote_addr}')
+    return Response('A WSGI generated this response!')
+
+#Running the server 
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    run_simple(
+        hostname = 'localhost',
+        port = 5555,
+        application = application
+    )
+    
+   
+
+
